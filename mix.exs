194c0a0b21b6e66a63fba901889107cf6f1d5a57
Thiago Santos <thia.mdossantos@gmail.com>
2020-04-11 13:16:29 -0300

    Drop support for elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BrazilianDocuments.MixProject do
     [
       app: :brazilian_documents,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Validate, format and valid brazilian CPFs and CNPJs",
